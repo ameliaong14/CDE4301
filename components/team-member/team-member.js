7c13b50dcf8b199aaa7c2659f122e32be12e0104
Amelia <ameliaojw@gmail.com>
2025-11-14 20:50:49 +0800

    front page v3 with team images

--- components/team-member/team-member.js
+++ components/team-member/team-member.js
@@ -18,9 +18,9 @@ class TeamMember extends HTMLElement {
 
   render() {
     const isPrimary = this.classList.contains('primary-author');
-    const imgSize = isPrimary ? '130px' : '100px';
-    const maxWidth = isPrimary ? '180px' : '120px';
-    const fontSize = isPrimary ? '1.15em' : '1em';
+    const imgSize = isPrimary ? '180px' : '140px';
+    const maxWidth = isPrimary ? '280px' : '240px';
+    const fontSize = isPrimary ? '1.2em' : '1.1em';
     
     const div = document.createElement("div");
     div.innerHTML = `
@@ -39,14 +39,29 @@ class TeamMember extends HTMLElement {
       img {
         width: ${imgSize};
         height: ${imgSize};
+        min-width: ${imgSize};
+        min-height: ${imgSize};
         border-radius: 50%;
         display: block;
         margin: auto;
+        object-fit: cover;
+        object-position: center center;
+        aspect-ratio: 1 / 1;
+        image-rendering: auto;
       }
 
       p {
         text-align: center;
         margin: 8px 0;
+      }
+      
+      p:nth-of-type(2) {
+        font-size: 0.85em;
+        color: #666;
+      }
+      
+      p:last-of-type:empty {
+        display: none;
       }
 
       @media (max-width: 599px) {
